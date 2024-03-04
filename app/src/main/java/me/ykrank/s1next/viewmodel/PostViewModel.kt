04c4100105e71567a1248bac3e778cbef5519dad
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2024-03-04 19:02:16 +0000

    Update PostViewModel.kt

--- app/src/main/java/me/ykrank/s1next/viewmodel/PostViewModel.kt
+++ app/src/main/java/me/ykrank/s1next/viewmodel/PostViewModel.kt
@@ -180,8 +180,11 @@ class PostViewModel(private val rxBus: RxBus, private val user: User) {
     fun onAppPostClick(v: View) {
         val p = post.get()
         val t = thread.get()
+        var p2 = p/30
+        var p3 = Math.ceil(p2)
+        var p4 = Double.valueOf(p3).intValue()
         if (p != null && t?.id != null) {
-            AppPostListActivity.start(v.context, t.id!!, p.getPage(), p.id.toString())
+            AppPostListActivity.start(v.context, t.id!!, p4, p.id.toString())
         }
     }
 
